cc68a3c17841b7d287c4f6863006ca938e3944d1
omershlo <omer.shlomovits@gmail.com>
2019-04-29 13:37:44 +0300

    lock

--- src/lib.rs
+++ src/lib.rs
@@ -14,17 +14,32 @@
 
     @license GPL-3.0+ <https://github.com/KZen-networks/sin-city/blob/master/LICENSE>
 */
+const SECURITY_BITS: usize = 256;
 
 extern crate curv;
 extern crate itertools;
 extern crate multi_party_ecdsa;
+extern crate paillier;
 
 mod test;
 
+use curv::arithmetic::traits::Modulo;
+use curv::arithmetic::traits::Samplable;
+use curv::cryptographic_primitives::commitments::hash_commitment::HashCommitment;
+use curv::cryptographic_primitives::commitments::traits::Commitment;
+use curv::cryptographic_primitives::hashing::hash_sha256::HSha256;
+use curv::cryptographic_primitives::hashing::traits::Hash;
 use curv::cryptographic_primitives::proofs::sigma_dlog::*;
+use curv::cryptographic_primitives::proofs::sigma_ec_ddh::NISigmaProof;
+use curv::cryptographic_primitives::proofs::sigma_ec_ddh::{
+    ECDDHProof, ECDDHStatement, ECDDHWitness,
+};
 use curv::elliptic::curves::traits::{ECPoint, ECScalar};
-use curv::{FE, GE};
+use curv::{BigInt, FE, GE};
 use itertools::iterate;
+use paillier::Paillier;
+use paillier::{Add, Decrypt, Encrypt, KeyGeneration, Mul};
+use paillier::{DecryptionKey, EncryptionKey, RawCiphertext, RawPlaintext};
 
 pub struct MultiHopLock {
     pub num_parties: usize,
@@ -43,6 +58,36 @@ pub struct ChainLink {
 pub struct ChainLinkUn {
     pub Y_i_minus_1: GE,
     pub k_n: FE,
+}
+
+pub struct LockParty0Message1 {
+    ddh_proof: ECDDHProof,
+    R_0: GE,
+    R_0_tag: GE,
+}
+
+pub struct LockParty1Message1 {
+    comm: BigInt,
+}
+
+pub struct DecommitLockParty1Message1 {
+    blind_factor: BigInt,
+    R_1: GE,
+    R_1_tag: GE,
+    ddh_proof: ECDDHProof,
+}
+
+pub struct PartialSig {
+    pub c_tag: BigInt,
+}
+
+pub struct LockParty1Message2 {
+    decomm: DecommitLockParty1Message1,
+    partial_sig: PartialSig,
+}
+
+pub struct LockParty0Message2 {
+    s_tag: FE,
 }
 
 impl MultiHopLock {
@@ -102,4 +147,199 @@ impl MultiHopLock {
             }
         }
     }
+}
+
+impl LockParty1Message1 {
+    pub fn first_message(
+        chain_link1: &ChainLink,
+    ) -> (FE, DecommitLockParty1Message1, LockParty1Message1) {
+        let g: GE = ECPoint::generator();
+
+        let r_1: FE = ECScalar::new_random();
+        let R_1 = g * &r_1;
+
+        let Y_1_tag: GE = chain_link1.Y_i_minus_1;
+        let w = ECDDHWitness { x: r_1.clone() };
+        let R_1_tag = &Y_1_tag * &r_1;
+        let delta = ECDDHStatement {
+            g1: g.clone(),
+            h1: R_1.clone(),
+            g2: Y_1_tag.clone(),
+            h2: R_1_tag.clone(),
+        };
+        let ddh_proof = ECDDHProof::prove(&w, &delta);
+
+        // we use hash based commitment
+        let pk_commitment_blind_factor = BigInt::sample(SECURITY_BITS);
+        // hash R1, R1_tag
+        let commit_hashed_message =
+            HSha256::create_hash_from_ge(&[&R_1, &R_1_tag, &ddh_proof.a1, &ddh_proof.a2]);
+
+        let commitment = HashCommitment::create_commitment_with_user_defined_randomness(
+            &commit_hashed_message.to_big_int(),
+            &pk_commitment_blind_factor,
+        );
+        let decommit = DecommitLockParty1Message1 {
+            blind_factor: pk_commitment_blind_factor,
+            R_1,
+            R_1_tag,
+            ddh_proof,
+        };
+        (r_1, decommit, LockParty1Message1 { comm: commitment })
+    }
+}
+
+impl LockParty0Message1 {
+    pub fn first_message(chain_link0: &ChainLink) -> (FE, LockParty0Message1) {
+        let g: GE = ECPoint::generator();
+        let r_0: FE = ECScalar::new_random();
+        let R_0 = &g * &r_0;
+        let Y_0: GE = chain_link0.Y_i;
+        let w = ECDDHWitness { x: r_0.clone() };
+        let R_0_tag = &Y_0 * &r_0;
+        let delta = ECDDHStatement {
+            g1: g.clone(),
+            h1: R_0.clone(),
+            g2: Y_0.clone(),
+            h2: R_0_tag.clone(),
+        };
+        let ddh_proof = ECDDHProof::prove(&w, &delta);
+        (
+            r_0,
+            LockParty0Message1 {
+                ddh_proof,
+                R_0,
+                R_0_tag,
+            },
+        )
+    }
+}
+
+impl LockParty1Message2 {
+    pub fn second_message(
+        lock_party0_message1: &LockParty0Message1,
+        decom_message1: DecommitLockParty1Message1,
+        ek: &EncryptionKey,
+        x_1: &FE,
+        encrypted_secret_share: &BigInt,
+        message: &BigInt,
+        r_1: &FE,
+        chain_link1: &ChainLink,
+    ) -> LockParty1Message2 {
+        // verify counter party NIZK:
+        let g: GE = ECPoint::generator();
+        let delta = ECDDHStatement {
+            g1: g,
+            h1: lock_party0_message1.R_0.clone(),
+            g2: chain_link1.Y_i_minus_1.clone(),
+            h2: lock_party0_message1.R_0_tag.clone(),
+        };
+        ECDDHProof::verify(&lock_party0_message1.ddh_proof, &delta).expect("bad NIZK");
+        // R
+        let R = lock_party0_message1.R_0_tag * r_1;
+        let q = FE::q();
+
+        let rx = R.x_coor().unwrap().mod_floor(&q);
+        let rho = BigInt::sample_below(&q.pow(2));
+        let r_1_inv = r_1.invert();
+        let partial_sig = rho * &q + BigInt::mod_mul(&r_1_inv.to_big_int(), message, &q);
+        let c1 = Paillier::encrypt(ek, RawPlaintext::from(partial_sig));
+        let v = BigInt::mod_mul(
+            &r_1_inv.to_big_int(),
+            &BigInt::mod_mul(&rx, &x_1.to_big_int(), &q),
+            &q,
+        );
+        let c2 = Paillier::mul(
+            ek,
+            RawCiphertext::from(encrypted_secret_share.clone()),
+            RawPlaintext::from(v),
+        );
+
+        let partial_sig = PartialSig {
+            c_tag: Paillier::add(ek, c2, c1).0.into_owned(),
+        };
+
+        LockParty1Message2 {
+            decomm: decom_message1,
+            partial_sig,
+        }
+    }
+}
+
+impl LockParty0Message2 {
+    pub fn second_message(
+        dk: &DecryptionKey,
+        lock_party1_message2: LockParty1Message2,
+        lock_party1_message1: LockParty1Message1,
+        message: &BigInt,
+        r_0: FE,
+        chain_link0: &ChainLink,
+        pubkey: &GE,
+    ) -> (FE, LockParty0Message2) {
+        // verify commitment:
+        let commit_hashed_message = HSha256::create_hash_from_ge(&[
+            &lock_party1_message2.decomm.R_1,
+            &lock_party1_message2.decomm.R_1_tag,
+            &lock_party1_message2.decomm.ddh_proof.a1,
+            &lock_party1_message2.decomm.ddh_proof.a2,
+        ]);
+        let commitment = HashCommitment::create_commitment_with_user_defined_randomness(
+            &commit_hashed_message.to_big_int(),
+            &lock_party1_message2.decomm.blind_factor,
+        );
+        assert_eq!(commitment, lock_party1_message1.comm);
+        // verify counter party NIZK:
+        let g: GE = ECPoint::generator();
+        let delta = ECDDHStatement {
+            g1: g,
+            h1: lock_party1_message2.decomm.R_1.clone(),
+            g2: chain_link0.Y_i.clone(),
+            h2: lock_party1_message2.decomm.R_1_tag.clone(),
+        };
+        ECDDHProof::verify(&lock_party1_message2.decomm.ddh_proof, &delta).expect("bad NIZK");
+        // R
+        let R = lock_party1_message2.decomm.R_1_tag * r_0;
+
+        let r_x = R.x_coor().unwrap().mod_floor(&FE::q());
+        let r_0_inv = r_0.invert();
+
+        let s = Paillier::decrypt(
+            dk,
+            &RawCiphertext::from(lock_party1_message2.partial_sig.c_tag),
+        );
+        let s_fe: FE = ECScalar::from(&s.0);
+        let s_R_1 = lock_party1_message2.decomm.R_1 * s_fe;
+        let r_x_fe: FE = ECScalar::from(&r_x);
+        let r_x_pk = pubkey * &r_x_fe;
+        let e_fe: FE = ECScalar::from(message);
+        let e_g = g * e_fe;
+        assert_eq!(s_R_1, e_g + r_x_pk);
+        let s_tag = s_fe.mul(&r_0_inv.get_element());
+        (s_tag, LockParty0Message2 { s_tag })
+    }
+
+    pub fn verify(
+        &self,
+        lock_party0_message1: LockParty0Message1,
+        r_1: &FE,
+        pubkey: &GE,
+        message: &BigInt,
+    ) -> FE {
+        let g: GE = ECPoint::generator();
+        let R = lock_party0_message1.R_0_tag * r_1;
+        let q = FE::q();
+        let r_x = R.x_coor().unwrap().mod_floor(&q);
+        let r_x_fe: FE = ECScalar::from(&r_x);
+        let r_x_pk = pubkey * &r_x_fe;
+        let s_tag_r_1_R_0 = lock_party0_message1.R_0 * r_1 * self.s_tag;
+        let e_fe: FE = ECScalar::from(message);
+        let e_g = g * e_fe;
+
+        assert_eq!(s_tag_r_1_R_0, e_g + r_x_pk);
+        self.s_tag.clone()
+    }
+}
+
+pub fn get_paillier_keys() -> (EncryptionKey, DecryptionKey) {
+    Paillier::keypair().keys()
 }
--- src/test.rs
+++ src/test.rs
@@ -13,11 +13,13 @@
 
     @license GPL-3.0+ <https://github.com/KZen-networks/sin-city/blob/master/LICENSE>
 */
-
 #[cfg(test)]
 mod tests {
 
     use MultiHopLock;
+
+    use multi_party_ecdsa::protocols::two_party_ecdsa::lindell_2017::party_one;
+    use multi_party_ecdsa::protocols::two_party_ecdsa::lindell_2017::party_two;
 
     #[test]
     fn test_setup() {
@@ -25,5 +27,154 @@ mod tests {
         let amhl = MultiHopLock::setup(n);
         MultiHopLock::verify_setup(&amhl.setup_chain[0]).expect("error");
         MultiHopLock::verify_setup(&amhl.setup_chain[1]).expect("error");
+    }
+
+    use curv::arithmetic::traits::Samplable;
+    use curv::elliptic::curves::traits::ECScalar;
+    use curv::{BigInt, FE};
+    use get_paillier_keys;
+    use LockParty0Message1;
+    use LockParty0Message2;
+    use LockParty1Message1;
+    use LockParty1Message2;
+
+    #[test]
+    fn test_lock() {
+        // 2p-keygen
+        // party0 in Lock protocol plays party_one . party1 in Lock protocol plays party_two
+        let random_third = BigInt::sample_below(&(FE::q() / BigInt::from(3)));
+        let secret_share_party_one: FE = ECScalar::from(&random_third);
+        let (party_one_first_message, comm_witness, ec_key_pair_party1) =
+            party_one::KeyGenFirstMsg::create_commitments_with_fixed_secret_share(
+                secret_share_party_one.clone(),
+            );
+
+        let secret_share_party_two: FE = ECScalar::new_random();
+        let (party_two_first_message, ec_key_pair_party2) =
+            party_two::KeyGenFirstMsg::create_with_fixed_secret_share(
+                secret_share_party_two.clone(),
+            );
+        let party_one_second_message = party_one::KeyGenSecondMsg::verify_and_decommit(
+            comm_witness,
+            &party_two_first_message.d_log_proof,
+        )
+        .expect("failed to verify and decommit");
+
+        let _party_two_second_message =
+            party_two::KeyGenSecondMsg::verify_commitments_and_dlog_proof(
+                &party_one_first_message,
+                &party_one_second_message,
+            )
+            .expect("failed to verify commitments and DLog proof");
+
+        // init paillier keypair:
+        let (ek, dk) = get_paillier_keys();
+        let paillier_key_pair =
+            party_one::PaillierKeyPair::generate_encrypted_share_from_fixed_paillier_keypair(
+                &ek,
+                &dk,
+                &ec_key_pair_party1,
+            );
+
+        let party_one_private =
+            party_one::Party1Private::set_private_key(&ec_key_pair_party1, &paillier_key_pair);
+
+        let party_two_paillier = party_two::PaillierPublic {
+            ek: paillier_key_pair.ek.clone(),
+            encrypted_secret_share: paillier_key_pair.encrypted_share.clone(),
+        };
+
+        let correct_key_proof =
+            party_one::PaillierKeyPair::generate_ni_proof_correct_key(&paillier_key_pair);
+        party_two::PaillierPublic::verify_ni_proof_correct_key(
+            correct_key_proof,
+            &party_two_paillier.ek,
+        )
+        .expect("bad paillier key");
+        // zk proof of correct paillier key
+
+        // zk range proof
+        let range_proof = party_one::PaillierKeyPair::generate_range_proof(
+            &paillier_key_pair,
+            &party_one_private,
+        );
+        let _result =
+            party_two::PaillierPublic::verify_range_proof(&party_two_paillier, &range_proof)
+                .expect("range proof error");
+
+        // pdl proof minus range proof
+        let (party_two_pdl_first_message, pdl_chal_party2) =
+            party_two_paillier.pdl_challenge(&party_one_second_message.comm_witness.public_share);
+
+        let (party_one_pdl_first_message, pdl_decommit_party1) =
+            party_one::PaillierKeyPair::pdl_first_stage(
+                &party_one_private,
+                &party_two_pdl_first_message,
+            );
+
+        let party_two_pdl_second_message =
+            party_two::PaillierPublic::pdl_decommit_c_tag_tag(&pdl_chal_party2);
+        let party_one_pdl_second_message = party_one::PaillierKeyPair::pdl_second_stage(
+            &party_one_pdl_first_message,
+            &party_two_pdl_first_message,
+            &party_two_pdl_second_message,
+            party_one_private.clone(),
+            pdl_decommit_party1,
+        )
+        .expect("pdl error party2");
+
+        party_two::PaillierPublic::verify_pdl(
+            &pdl_chal_party2,
+            &party_one_pdl_first_message,
+            &party_one_pdl_second_message,
+        )
+        .expect("pdl error party1");
+
+        //compute public key party_one:
+        let pubkey_party_one =
+            party_one::compute_pubkey(&party_one_private, &party_two_first_message.public_share);
+
+        //compute public key party_two:
+        let pubkey_party_two = party_two::compute_pubkey(
+            &ec_key_pair_party2,
+            &party_one_second_message.comm_witness.public_share,
+        );
+
+        assert_eq!(pubkey_party_one, pubkey_party_two);
+        // instead of using the original Lindell ephemeral key generation we will use amhl version:
+        let n: usize = 3;
+        let amhl = MultiHopLock::setup(n);
+
+        let (r_1, decommit, lock_party1_message1) =
+            LockParty1Message1::first_message(&amhl.setup_chain[1]);
+
+        let (r_0, lock_party0_message1) = LockParty0Message1::first_message(&amhl.setup_chain[0]);
+
+        let message = BigInt::from(2); //TODO: what is the message ?
+
+        let lock_party1_message2 = LockParty1Message2::second_message(
+            &lock_party0_message1,
+            decommit,
+            &party_two_paillier.ek,
+            &secret_share_party_two,
+            &party_two_paillier.encrypted_secret_share,
+            &message,
+            &r_1,
+            &amhl.setup_chain[1],
+        );
+
+        let (_s_tag_party0, lock_party0_message2) = LockParty0Message2::second_message(
+            &dk,
+            lock_party1_message2,
+            lock_party1_message1,
+            &message,
+            r_0,
+            &amhl.setup_chain[0],
+            &pubkey_party_one,
+        );
+
+        // party1_output:
+        let _s_tag_party1 =
+            lock_party0_message2.verify(lock_party0_message1, &r_1, &pubkey_party_two, &message);
     }
 }
